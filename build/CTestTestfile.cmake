f7f22eeed6b6c46f6f9a60285c4b4ff57caac54d
Oscar Lima <olima_84@yahoo.com>
2014-03-14 20:56:27 +0100

    deleting build folders

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eren/ros/hydro/youbot_ws/src/binary_bitbots/src
-# Build directory: /home/eren/ros/hydro/youbot_ws/src/binary_bitbots/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(basic_navigation)
--- build/catkin_generated/order_packages.py
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/eren/ros/hydro/youbot_ws/src/binary_bitbots/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
